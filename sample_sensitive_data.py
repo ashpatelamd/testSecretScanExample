9b7bb77388ddd963b1d30e08a4d74e7c54d0edda
ashpatelamd <140600898+ashpatelamd@users.noreply.github.com>
2024-11-14 13:14:15 +0530

    Create sample_sensitive_data.py
    this is dummy data

---
+++ sample_sensitive_data.py
@@ -0,0 +1,8 @@
+# Example: sample_sensitive_data.py
+#this is dummy data for testing secret scan tool
+
+api_key = '1234567890abcdefghijklmnopqrstuv'
+password = 'my_super_secret_password'
+
+def dummy_function():
+    print("This is just a dummy function.")
